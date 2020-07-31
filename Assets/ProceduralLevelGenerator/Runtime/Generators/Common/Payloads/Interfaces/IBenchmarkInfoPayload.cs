9943708ccd93eb60061b1e7e96eb23f309204fa0
rheaadur <68752538+rheaadur@users.noreply.github.com>
2020-07-31 14:07:28 -0400

    commited script folders

---
+++ Assets/ProceduralLevelGenerator/Runtime/Generators/Common/Payloads/Interfaces/IBenchmarkInfoPayload.cs
@@ -0,0 +1,18 @@
+﻿using ProceduralLevelGenerator.Unity.Generators.Common.Utils;
+
+namespace ProceduralLevelGenerator.Unity.Generators.Common.Payloads.Interfaces
+{
+    /// <summary>
+    /// Represents a payload with additional benchmark information.
+    /// </summary>
+    public interface IBenchmarkInfoPayload
+    {
+        GeneratedLevel GeneratedLevel { get; set; }
+
+        int Iterations { get; set; }
+
+        double TimeTotal { get; set; }
+
+        GeneratorStats GeneratorStats { get; set; }
+    }
+}
